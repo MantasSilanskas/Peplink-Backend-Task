566f83eb561bd521274578f49845d35aeb84da15
MantasSilanskas <mantas.silanskas.ms@gmail.com>
2020-10-15 15:46:44 +0300

    Added rulesFile.json, reading of rules file and updated gitignore file.

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,49 @@
 package main
 
-func main()  {
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
+type ruleSets struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Rules       []struct {
+		CryptoID string  `json:"crypto_id"`
+		Price    float64 `json:"price"`
+		Rule     string  `json:"rule"`
+	} `json:"rules"`
+}
 
-	
+func main() {
+
+	ruleSet, err := LoadRuleSets("rulesFile.json")
+	if err != nil {
+		panic(err)
+	}
+
+	for i, v := range ruleSet.Rules {
+
+		fmt.Println(v.Price)
+		i++
+
+	}
+
+}
+
+func LoadRuleSets(filename string) (ruleSets, error) {
+
+	var ruleSet ruleSets
+	ruleSetFile, err := os.Open(filename)
+	defer ruleSetFile.Close()
+	if err != nil {
+		return ruleSet, err
+	}
+	jsonParser := json.NewDecoder(ruleSetFile)
+	err = jsonParser.Decode(&ruleSet)
+	if err != nil {
+		return ruleSet, err
+	}
+	return ruleSet, err
 }
